ea6214da8c301b185996bc65a2b2cc1064a4d8cd
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-05 21:27:33 +0530

    Epi -04 - useEffect - variation 01

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [text,setText] = useState('');
+  useEffect(()=>{
+    console.log('UI rendering done')
+  },[]);
+  function changeHandler(eve){
+    console.log(text);
+    setText(eve.target.value);
+  }
+  return (
+    <div className="App">
+      <input type="text" onChange={changeHandler}></input>
+    </div>
+  );
+}
+
+export default App;
