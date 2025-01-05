062eeec2aa92f857c8dd3ba744bd6b16c90d22ca
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-05 21:34:43 +0530

    Epi - 04 - var 01 without dependency array render each time,  variation 02 - when dependency array declare- only first time render

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import { useEffect, useState } from 'react';
 function App() {
   const [text,setText] = useState('');
 
-  //variation 01 - every rendering use effect call
+  //variation 01 - every rendering call each time call but no dependency [] parameter
+  // useEffect(()=>{
+  //   console.log('UI rendering done')
+  // });
+
+  //variation 02 - first render - only first time print
   useEffect(()=>{
     console.log('UI rendering done')
   },[]);
