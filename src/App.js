a48eecde7d198748e9e1683b61b061a802f81311
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-05 21:30:11 +0530

    Epi -04 - useEffect - variation 01 every change useeffect render- bad practice

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [text,setText] = useState('');
+
+  //variation 01 - every rendering use effect call
   useEffect(()=>{
     console.log('UI rendering done')
   },[]);
+
   function changeHandler(eve){
     console.log(text);
     setText(eve.target.value);
